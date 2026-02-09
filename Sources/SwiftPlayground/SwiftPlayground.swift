efc6ba0bc618540183841b905c9e417981ad2293
milliearmstrong-glitch <millie.armstrong@student.onslow.school.nz>
2026-02-09 02:00:58 +0000

    goodbye

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,54 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-func totalCost(lunches:[Double]) -> Double{
-    var cost = 0.0
-        for day in lunches{
-            cost += day
-        }
-    return cost
-}
 
-func budgetCheck(totalCost: Double, budget: Double) -> Bool {
-    if totalCost > budget {
-        return false
-    } else{
-        return true
-    }
-}
 @main
 
 struct SwiftPlayground {
     static func main() {
-        let budget = 35.0
-        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
-        var lunchTotal = 0.0
-        //let weekDays = ["monday", "tuesday", "wesnday", "thursday", "friday"]
-        var counter = 1
-        for day in lunches{
-            lunchTotal += day
-            print("day \(counter): $\(day)")
-            counter += 1
-            if day >= 9{
-                print("High spending day detected")
-            }
-        }
-        var snack_total = 0.0
-        let snack_cost = 2.5
-        while (snack_total < 10){
-            snack_total += snack_cost
-            print("Snack total: $\(snack_total)")
-        }
-
-                print("Lunch total: $\(lunchTotal)")
-        print("Snack total: $\(snack_total)")
-        print("Combined total: $\(snack_total+lunchTotal)")
-        print("Average lunch cost: $\(lunchTotal/5)")
-        if (budgetCheck(totalCost: totalCost(lunches:lunches), budget: budget)) {
-            print("you stayed in budget")
-        }
-        else{
-            print("Warning: You overspent this week")
-        }
-
+        let words = ["apple", "cat", "banana", "dog", "grape", "kiwi"]
+        let myWords = words.filter { word in
+        return word.count > 4 }
+        print(myWords)
+        let numbers = [7, 14, 21, 28, 35]
+        let total = numbers.reduce(0) { Result, number in return Result + number}
+        print (total)
     }
 }
