97f079faa61f288c3ca24c75669b8cc8fd5a020d
milliearmstrong-glitch <millie.armstrong@student.onslow.school.nz>
2026-02-10 23:44:00 +0000

    byeee

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,11 +6,23 @@
 struct SwiftPlayground {
     static func main() {
         let words = ["apple", "cat", "banana", "dog", "grape", "kiwi"]
+        
         let myWords = words.filter { word in
         return word.count > 4 }
         print(myWords)
         let numbers = [7, 14, 21, 28, 35]
         let total = numbers.reduce(0) { Result, number in return Result + number}
         print (total)
+
+        let foods = ["apple", "banana", "grape", "strawberry", "kiwi"]
+        let long = foods.reduce(foods[0]) { result, food in
+            return max(result, food)
+        }
+        print(long)
+
+        let sq = numbers.map { number in 
+        return number * number 
+        }
+        print (sq)
     }
 }
