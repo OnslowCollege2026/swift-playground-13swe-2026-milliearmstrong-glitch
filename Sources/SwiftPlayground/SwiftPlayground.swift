47fc5afd245144a0e208ec731ce28b32c83e82d7
milliearmstrong-glitch <millie.armstrong@student.onslow.school.nz>
2026-04-23 23:09:55 +0000

    retuns

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -3,7 +3,8 @@
 //this is the welcome mesage
 let wellcome = "wellcome to the 'Told You So Libry', where every book tried to warn you, what are you looking to do today?"
 //these are all the options the user has to pick from
-let options = "A.Borow \n B.Return \n C.regerster as a user \n D.Veiw our catalog \n E.Serch for a spesific user \n F.serch for a specific book \n G.Add an Item \n H. Remove an item \n I.Edit an item \n J.Edit borrower records"
+let options = "A.Borow \nB.Return\nC.regerster as a user \nD.Veiw our catalog \nE.Serch for a spesific user \nF.serch for a specific book \nG.Add an Item \nH.Remove an item \nI.Edit an item \nJ.Edit borrower records"
+
 struct Books{
     let id: Int
     var bookTittle: String
@@ -25,9 +26,22 @@ struct SwiftPlayground {
         var bookList = [Books(id: 1, bookTittle: "1984", bookAuther: "George Orwell", copys: 3, copysAvalable: 3), Books(id: 2, bookTittle: "the hunger games", bookAuther: "Suzanne Collins", copys: 5, copysAvalable: 4), Books(id: 3, bookTittle: "fahrenheit 451", bookAuther: "Ray Bradbury", copys: 1, copysAvalable: 1), Books(id: 4, bookTittle: "late capitalism", bookAuther: "Ernest Mandel", copys: 2, copysAvalable: 0), Books(id: 5, bookTittle: "the handmaid's tale", bookAuther: "Margaret Atwood",  copys: 2, copysAvalable: 2), Books(id: 6, bookTittle: "I have no mouth and I must scream", bookAuther: "Harlan Ellison", copys: 1, copysAvalable: 1)]
         
         //this is a list of people who are already regrested with the libray
-        var clinets = [ Borower(id: 1, name: "Dave", bookId: 2),  Borower(id: 2, name: "Leb", bookId: 4), Borower(id: 3, name: "Rose", bookId: 4), Borower(id: 4, name: "Scout", bookId: 0) ]
-    print(wellcome)
-    print(options)
-    
+        var users = [ Borower(id: 1, name: "Dave", bookId: 2),  Borower(id: 2, name: "Leb", bookId: 4), Borower(id: 3, name: "Rose", bookId: 4), Borower(id: 4, name: "Scout", bookId: 0) ]
+        func returning(users: [String]) -> [String]{
+        print("what is your acount name?")
+        let name = readLine()!
+        return users.filter{$0.name}
     }
-}
+    var loop = true
+    print(wellcome)
+    let letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    while loop{
+    print(options)
+    let line = readLine()!.lowercased()
+    let userImput = letterOptions.filter {$0 == line}
+    if userImput != []{
+        loop = false
+        
+    }
+    }
+}}
