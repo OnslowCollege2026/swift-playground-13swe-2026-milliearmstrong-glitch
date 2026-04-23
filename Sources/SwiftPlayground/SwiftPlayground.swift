abff0a1dd3ad593f29de94dbc0417ebd370b4733
milliearmstrong-glitch <millie.armstrong@student.onslow.school.nz>
2026-04-23 22:06:36 +0000

    removed magic string

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,5 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+//this is the welcome mesage
+let wellcome = "wellcome to the 'Told You So Libry', where every book tried to warn you, what are you looking to do today?"
+//these are all the options the user has to pick from
+let options = "A.Borow \n B.Return \n C.regerster as a user \n D.Veiw our catalog \n E.Serch for a spesific user \n F.serch for a specific book \n G.Add an Item \n H. Remove an item \n I.Edit an item \n J.Edit borrower records"
 struct Books{
     let id: Int
     var bookTittle: String
@@ -17,13 +21,13 @@ struct Borower{
 
 struct SwiftPlayground {
     static func main() {
-        //this is my starting colection of books for the "We told you so libray", where every warns you about what you already let happen
+        //this is my starting colection of books for the "We told you so libray", where every book warns you about what you already let happen
         var bookList = [Books(id: 1, bookTittle: "1984", bookAuther: "George Orwell", copys: 3, copysAvalable: 3), Books(id: 2, bookTittle: "the hunger games", bookAuther: "Suzanne Collins", copys: 5, copysAvalable: 4), Books(id: 3, bookTittle: "fahrenheit 451", bookAuther: "Ray Bradbury", copys: 1, copysAvalable: 1), Books(id: 4, bookTittle: "late capitalism", bookAuther: "Ernest Mandel", copys: 2, copysAvalable: 0), Books(id: 5, bookTittle: "the handmaid's tale", bookAuther: "Margaret Atwood",  copys: 2, copysAvalable: 2), Books(id: 6, bookTittle: "I have no mouth and I must scream", bookAuther: "Harlan Ellison", copys: 1, copysAvalable: 1)]
         
         //this is a list of people who are already regrested with the libray
         var clinets = [ Borower(id: 1, name: "Dave", bookId: 2),  Borower(id: 2, name: "Leb", bookId: 4), Borower(id: 3, name: "Rose", bookId: 4), Borower(id: 4, name: "Scout", bookId: 0) ]
-    print("wellcome to the 'Told You So Libry', where every book tried to warn you, what are you looking to do today?")
-    print("A.Borow \n B.Return \n C.regerster as a user \n D.Veiw our catalog \n E.Serch for a spesific user \n F.serch for a specific book \n G.Add an Item \n H. Remove an item \n I.Edit an item \n J.Edit borrower records")
+    print(wellcome)
+    print(options)
     
     }
 }
