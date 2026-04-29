cf37a73b6c32642afdc9def7709c20d291a805f6
milliearmstrong-glitch <millie.armstrong@student.onslow.school.nz>
2026-04-29 00:20:54 +0000

    ah

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -16,26 +16,30 @@ struct Borower{
     let id: Int
     var name: String
     var bookId: Int
-}
+            func returning() -> Int{
+        print("what is your acount name?")
+        let userName = readLine()!
+        if userName == name{
+            return id
+        }
+        else{
+            return 0
+        }
+}}
 @main
 
 
 struct SwiftPlayground {
     static func main() {
+        
         //this is my starting colection of books for the "We told you so libray", where every book warns you about what you already let happen
         var bookList = [Books(id: 1, bookTittle: "1984", bookAuther: "George Orwell", copys: 3, copysAvalable: 3), Books(id: 2, bookTittle: "the hunger games", bookAuther: "Suzanne Collins", copys: 5, copysAvalable: 4), Books(id: 3, bookTittle: "fahrenheit 451", bookAuther: "Ray Bradbury", copys: 1, copysAvalable: 1), Books(id: 4, bookTittle: "late capitalism", bookAuther: "Ernest Mandel", copys: 2, copysAvalable: 0), Books(id: 5, bookTittle: "the handmaid's tale", bookAuther: "Margaret Atwood",  copys: 2, copysAvalable: 2), Books(id: 6, bookTittle: "I have no mouth and I must scream", bookAuther: "Harlan Ellison", copys: 1, copysAvalable: 1)]
         
         //this is a list of people who are already regrested with the libray
         var users = [ Borower(id: 1, name: "Dave", bookId: 2),  Borower(id: 2, name: "Leb", bookId: 4), Borower(id: 3, name: "Rose", bookId: 4), Borower(id: 4, name: "Scout", bookId: 0) ]
-        func returning(users: [any]) -> [String]{
-        print("what is your acount name?")
-        let userName = readLine()!
-        for user in users{
-            user.name
-        }
+
             
-        }
-    }
+
     var loop = true
     print(wellcome)
     let letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
@@ -47,5 +51,7 @@ struct SwiftPlayground {
         loop = false
         
     }
+    (users.name).filter
+    users[1].returning()
     }
 }}
