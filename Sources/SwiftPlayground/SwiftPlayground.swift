cee66eb3038d276ba8939898466a7a7b3df99e78
milliearmstrong-glitch <millie.armstrong@student.onslow.school.nz>
2026-05-03 09:49:25 +0000

    sunday

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -22,40 +22,75 @@ struct Borower: Equatable{
 
 struct SwiftPlayground {
     static func main() {
-        
+        func catolog(){
+                        var loop = 0
+            var listing = ["Place holder"]
+            for book in bookList{
+                loop += 1
+                listing.append("\(loop).\(book.bookTittle)")
+            }
+            listing.remove(at: 0)
+            for list in listing{
+                print(list)
+            }
+            
+
+
+            }
         //this is my starting colection of books for the "We told you so libray", where every book warns you about what you already let happen
         var bookList = [Books(id: 1, bookTittle: "1984", bookAuther: "George Orwell", copys: 3, copysAvalable: 3), Books(id: 2, bookTittle: "the hunger games", bookAuther: "Suzanne Collins", copys: 5, copysAvalable: 4), Books(id: 3, bookTittle: "fahrenheit 451", bookAuther: "Ray Bradbury", copys: 1, copysAvalable: 1), Books(id: 4, bookTittle: "late capitalism", bookAuther: "Ernest Mandel", copys: 2, copysAvalable: 0), Books(id: 5, bookTittle: "the handmaid's tale", bookAuther: "Margaret Atwood",  copys: 2, copysAvalable: 2), Books(id: 6, bookTittle: "I have no mouth and I must scream", bookAuther: "Harlan Ellison", copys: 1, copysAvalable: 1)]
         
         //this is a list of people who are already regrested with the libray
         var users = [ Borower(id: 1, name: "Dave", bookId: 2),  Borower(id: 2, name: "Leb", bookId: 4), Borower(id: 3, name: "Rose", bookId: 4), Borower(id: 4, name: "Scout", bookId: 0) ]
+        //var numList:[String]
+
     func nameCheack(use: [String]){
     var nameInvaled = true
         print("please enter your name")
     while nameInvaled == true{
 
     let userName = readLine()!.lowercased()
-    let M_Ws = users.filter { seen in return (seen.name) == userName}
+    let M_Ws = users.filter { seen in return (seen.name).lowercased() == userName}
     if M_Ws != []{
         nameInvaled = false
-            if use == ["b"]{
-        print("your copy of")
-        print(bookList.filter { seen in return (seen.id) == M_Ws[0].bookId})
+        if use == ["a"]{
+            catolog()
+        let test = (readLine()!).lowercased()
+        var vailedChoice = true
+        for chr in ("abcdefghijklmnopqrstuvwxyz"){
+            if test.contains(chr){
+                vailedChoice = false
+            }
+        }
+        if vailedChoice{
+            let choice = Int(test)!
+            if choice <= bookList.count && choice >= 1{
+                print((bookList[choice-1]).bookTittle)
+        }
+        }
+        else if use == ["b"]{
+            switch M_Ws[0].bookId
+            {
+                case 0:
+                print("you have no books to return")
+                default:
+                let bookEntry = (bookList.filter { seen in return (seen.id) == M_Ws[0].bookId})
+                print("Your copy of \(bookEntry[0].bookTittle) has been returned")
+            }
 
 
-        users = users.filter { seen in return (seen.name) != userName}
-        users.append(Borower(id: M_Ws[0].id, name: M_Ws[0].name, bookId: 0))
 
-        print(users)
+            users = users.filter { seen in return (seen.name).lowercased() != userName}
+            users.append(Borower(id: M_Ws[0].id, name: M_Ws[0].name, bookId: 0))
+
         
     }
-    else{
+
+    }    else{
         print("sorry we could not find you in our system, please enter your name again")
     }
-    }}
-
     }
-            
-
+    }}
     var loop = true
     print(wellcome)
     let letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
