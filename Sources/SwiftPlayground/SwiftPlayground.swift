06d014a11c7013a2b5d360f8057903dbf4e89d4f
milliearmstrong-glitch <millie.armstrong@student.onslow.school.nz>
2026-05-08 11:42:51 +0000

    May 8, 2026, 11:42 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -119,7 +119,7 @@ struct SwiftPlayground {
             case 0: copysChange = 1
             default: copysChange = -1
         }
-        
+        users[]
         
         users = users.filter { user in return (user.name) != (M_Ws[0].name)} 
         users.append(Borower(id: M_Ws[0].id, name: M_Ws[0].name, bookId: bookId))
